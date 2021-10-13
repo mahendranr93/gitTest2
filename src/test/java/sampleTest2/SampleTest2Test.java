cefea09f5b7811414814cf25a8ba29086016f9a9
mahendranr93 <mahendranr93@gmail.com>
2021-10-13 07:13:30 +0530

    first commit

---
+++ src/test/java/sampleTest2/SampleTest2Test.java
@@ -0,0 +1,38 @@
+package sampleTest2;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SampleTest2Test {
+	
+	@Test
+	public void loginTest() throws IOException, InterruptedException{
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver wd=new ChromeDriver();
+		wd.manage().window().maximize();
+		wd.get("https://www.bing.com/");
+
+		File src= ((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+
+		///Users/e11/Documents/online/workspace/batch181/C2_WebdriverTest/screenshot
+		//FileUtils //appache //1.0, 2.0
+		FileHandler.copy(src, new File("C:\\vcentry\\SeleniumWorkspace\\gitTest2\\screenshot\\bing.png") );
+
+		Thread.sleep(2000);
+		wd.quit();
+		
+	
+	}
+
+
+}
